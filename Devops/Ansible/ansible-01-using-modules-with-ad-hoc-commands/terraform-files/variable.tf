03ccb8f21da296851c8fcc1706bfe3359fbf79b7
Mergen-MA <malidinc13@gmail.com>
2023-04-24 22:26:35 +0200

    Ansible installation and ad-hoc command, configure inventory and hosts files

---
+++ Devops/Ansible/ansible-01-using-modules-with-ad-hoc-commands/terraform-files/variable.tf
@@ -0,0 +1,21 @@
+variable "tags" {
+  default = ["control_node", "node_1", "node_2"]
+}
+variable "mykey" {
+  default = "clarusway"
+}
+variable "user" {
+  default = "clarusway"
+}
+
+variable "amznlnx2" {
+  default = "ami-05fa00d4c63e32376"
+}
+
+variable "ubuntu" {
+  default = "ami-08d4ac5b634553e16"
+}
+
+variable "instype" {
+  default = "t2.micro"
+}
