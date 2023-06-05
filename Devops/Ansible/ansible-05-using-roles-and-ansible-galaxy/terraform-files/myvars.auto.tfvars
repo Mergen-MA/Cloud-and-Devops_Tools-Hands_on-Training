df2c18a9e0efd943c87034294a6b0cb9307be085
Mergen-MA <malidinc13@gmail.com>
2023-06-05 11:59:02 +0200

    ansible-05-using-roles-and-ansible-galaxy

---
+++ Devops/Ansible/ansible-05-using-roles-and-ansible-galaxy/terraform-files/myvars.auto.tfvars
@@ -0,0 +1,9 @@
+//aws_access_key = ""
+//aws_secret_key = ""
+myami = ["ami-02396cdd13e9a1257", "ami-016eb5d644c333ccb", "ami-007855ac798b5175e"]
+mykey = "clarusway"
+tags = ["control_node", "web_server_1", "web_server_2"]
+instancetype = "t2.micro"
+num = 3
+region = "us-east-1"
+mysecgr = "ansible-session5-sec-gr"
---
+++ Devops/Ansible/ansible-05-using-roles-and-ansible-galaxy/terraform-files/variables.tf
@@ -0,0 +1,11 @@
+//variable "aws_secret_key" {}
+//variable "aws_access_key" {}
+variable "region" {}
+variable "mykey" {}
+variable "tags" {}
+variable "myami" {
+  description = "in order of; amazon linux 2023, redhat enterprise linux 9, ubuntu 22.04"
+}
+variable "instancetype" {}
+variable "num" {}
+variable "mysecgr" {}
